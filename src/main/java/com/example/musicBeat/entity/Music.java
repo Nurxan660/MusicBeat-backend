bceff11b60d6a58c7ea4b2e134e6127b856a8a7a
Nurxan <ertaev.nurxan@bk.ru>
2022-05-06 03:25:04 +0600

    changes

---
+++ src/main/java/com/example/musicBeat/entity/Music.java
@@ -0,0 +1,66 @@
+package com.example.musicBeat.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Music {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long musicId;
+    private String url;
+    private String name;
+    private String author;
+    private String duration;
+    private String album;
+
+    public Long getMusicId() {
+        return musicId;
+    }
+
+    public void setMusicId(Long musicId) {
+        this.musicId = musicId;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+}
