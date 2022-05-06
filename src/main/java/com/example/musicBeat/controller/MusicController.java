25ccd2f21154a0b84165a4973611481af45aa85a
Nurxan <ertaev.nurxan@bk.ru>
2022-05-06 11:45:42 +0600

    f

--- src/main/java/com/example/musicBeat/controller/MusicController.java
+++ src/main/java/com/example/musicBeat/controller/MusicController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/music")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class MusicController {
 
     @Autowired
